fc80fa549833c9bb8a1458b7c168bca0f38e7029
KShraddha-angularminds <shraddha.karpe@angularminds.in>
2022-07-26 14:43:42 +0530

    initial commit with header compoent

---
+++ src/components/Dropdown.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+function Dropdown() {
+  const [planet, setPlanet] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://findfalcone.herokuapp.com/planets")
+      .then((res) => {
+        console.log(res.data);
+        setPlanet(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(planet);
+  return <div>dropdown</div>;
+}
+
+export default Dropdown;
